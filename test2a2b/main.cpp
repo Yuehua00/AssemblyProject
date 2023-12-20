7aa9e65d6cc9729f2509fbc4fe6353b7642810fc
x10101 <elaine9210@gmail.com>
2023-12-20 20:14:43 +0800

    update c++

--- test2a2b/main.cpp
+++ test2a2b/main.cpp
@@ -2,37 +2,32 @@
 using namespace std;
 
 #define SIZE 4
-#define border "---------------------------------"
+#define border "-------------------------------------"
 
 int game[SIZE][SIZE];
 int Row = 4, Col = 4;  // 可以以輸入更改
 bool full = false;
 
-void resetGame()
-{
+void resetGame() {
     for (int i = 0; i < SIZE; i++)
         for (int j = 0; j < SIZE; j++)
             game[i][j] = 0;
 }
 
-void print()
-{
-    for (int i = 0; i < SIZE; i++)
-    {
+void print() {
+    for (int i = 0; i < SIZE; i++) {
         cout << border << endl;
 
-        for (int j = 0; j < SIZE; j++)
-        {
-            if (!game[i][j]) cout << "|   \t";
-            else cout << "|   " << game[i][j] << "\t";
+        for (int j = 0; j < SIZE; j++) {
+            if (!game[i][j]) printf("|%8s", " ");
+            else printf("|%8d", game[i][j]);
         }
         cout << "|" << endl;
     }
     cout << border << endl;
 }
 
-int Game_now()    // 遊戲狀況
-{
+int Game_now() {    // 遊戲狀況
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             if (game[i][j] == 2048) return 2;
@@ -56,8 +51,7 @@ int Game_now()    // 遊戲狀況
     return 0;
 }
 
-bool CreateNumber()
-{
+bool CreateNumber() {
     int x, y;
     int probablity = rand() % 3;    // 先跟她一樣，後面看要不要修改
 
@@ -76,31 +70,24 @@ bool CreateNumber()
     return true;
 }
 
-int Process(char dir)
-{
+int Process(char dir) {
     int change = 0;
     int check[4];
     for (int i = 0; i < 4; i++) check[i] = 0;
 
-    switch(dir)
-    {
+    switch(dir) {
     case 'w':  // up
-        for(int row = 1; row < Row; row++)   //最上面一行不動所以從1開始
-        {
-            for(int crow = row; crow >= 1; --crow)   // 不懂
-            {
-                for(int col = 0; col < Col; col++)
-                {
-                    if(game[crow-1][col] == 0)          // 上一排為空
-                    {
+        for(int row = 1; row < Row; row++) {   //最上面一行不動所以從1開始
+            for(int crow = row; crow >= 1; --crow) {   // 不懂
+                for(int col = 0; col < Col; col++) {
+                    if(game[crow-1][col] == 0) {          // 上一排為空
                         game[crow-1][col] = game[crow][col];
                         game[crow][col] = 0;
+
                         if (game[crow-1][col]) change++;
                     }
-                    else
-                    {
-                        if(game[crow-1][col] == game[crow][col] && check[crow] == 0)    // 合併
-                        {
+                    else {
+                        if(game[crow-1][col] == game[crow][col] && check[crow] == 0) {    // 合併
                             game[crow-1][col] *= 2;
                             game[crow][col] = 0;
 
@@ -112,24 +99,19 @@ int Process(char dir)
             }
         }
         break;
+
     case 's':  // down
-        for(int row = Row-2; row >= 0; row--)    //最下面一行不動
-        {
-            for(int crow = row; crow < Row-1; crow++)
-            {
-                for(int col = 0; col < Col; col++)
-                {
-                    if(game[crow+1][col] == 0)
-                    {
+        for(int row = Row-2; row >= 0; row--) {    //最下面一行不動
+            for(int crow = row; crow < Row-1; crow++) {
+                for(int col = 0; col < Col; col++) {
+                    if(game[crow+1][col] == 0) {
                         game[crow+1][col] = game[crow][col];
                         game[crow][col] = 0;
 
                         if (game[crow+1][col]) change++;
                     }
-                    else
-                    {
-                        if(game[crow+1][col] == game[crow][col] && check[crow] == 0)
-                        {
+                    else {
+                        if(game[crow+1][col] == game[crow][col] && check[crow] == 0) {
                             game[crow+1][col] *= 2;
                             game[crow][col] = 0;
 
@@ -141,24 +123,19 @@ int Process(char dir)
             }
         }
         break;
+
     case 'a':    // left
-        for(int col = 1; col < Col; col++)
-        {
-            for(int ccol = col; ccol >= 1; ccol--)
-            {
-                for(int row = 0; row < Row; row++)
-                {
-                    if(game[row][ccol-1] == 0)
-                    {
+        for(int col = 1; col < Col; col++) {
+            for(int ccol = col; ccol >= 1; ccol--) {
+                for(int row = 0; row < Row; row++) {
+                    if(game[row][ccol-1] == 0) {
                         game[row][ccol-1] = game[row][ccol];
                         game[row][ccol] = 0;
 
                         if (game[row][ccol-1]) change++;
                     }
-                    else
-                    {
-                        if(game[row][ccol-1] == game[row][ccol] && check[ccol] == 0)
-                        {
+                    else {
+                        if(game[row][ccol-1] == game[row][ccol] && check[ccol] == 0) {
                             game[row][ccol-1] *= 2;
                             game[row][ccol] = 0;
 
@@ -170,21 +147,18 @@ int Process(char dir)
             }
         }
         break;
+
     case 'd':  // right
-        for(int col = Col - 2; col >= 0; col--)
-        {
-            for(int ccol = col; ccol <= Col - 2; ccol++)
-            {
-                for(int row = 0; row < Row; row++)
-                {
-                    if(game[row][ccol + 1] == 0)
-                    {
+        for(int col = Col - 2; col >= 0; col--) {
+            for(int ccol = col; ccol <= Col - 2; ccol++) {
+                for(int row = 0; row < Row; row++) {
+                    if(game[row][ccol + 1] == 0) {
                         game[row][ccol + 1] = game[row][ccol];
                         game[row][ccol] = 0;
 
                         if (game[row][ccol+1]) change++;
                     }
-                    else{
+                    else {
                         if(game[row][ccol + 1] == game[row][ccol] && check[ccol] == 0){
                             game[row][ccol + 1] *= 2;
                             game[row][ccol] = 0;
@@ -202,15 +176,22 @@ int Process(char dir)
     return change;
 }
 
-int main()
-{
-    char play = 'Y';
+int main() {
+    // 輸出開始訊息
+    cout << "Welcome to the world of 2048! ; )" << endl << endl;
+    cout << "Ready for some blocky adventures?" << endl;
+    cout << "Use 'w' to make blocks ascend like magic!" << endl;
+    cout << "Use 's' to send them on a downward dance!" << endl;
+    cout << "Use 'a' to make blocks boogie left!" << endl;
+    cout << "Use 'd' to direct blocks in a rightward rave!" << endl;
+    cout << "Let the games begin! May the merging madness unfold!" << endl << endl;
+
     srand((unsigned int)time(0));
+    char play = 'Y';
     char direction;
     int gameState;
 
-    while(play == 'Y')
-    {
+    while(play == 'Y') {
         resetGame();
         CreateNumber();
         CreateNumber();
@@ -219,10 +200,14 @@ int main()
 
         while(gameState == 1) {
             cin >> direction;
-            if (Process(direction) > 0)
+            if (Process(direction) > 0) {
                 CreateNumber();
-            print();
-            gameState = Game_now();
+                print();
+                gameState = Game_now();
+            }
+            else {
+                cout << "Please enter again." << endl;
+            }
         }
 
         if(gameState == 0)
@@ -234,7 +219,6 @@ int main()
         cin >> play;
     }
     cout << "Thanks for playing!" << endl;
-
 
     return 0;
 }
